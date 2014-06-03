73a5f8fc2cc43af3f1900d9e28daee411a4dfcd5
alexandre.tavares <alexandre.tavares@jexperts.com.br>
2014-06-03 17:23:58 -0300

    teste

--- app.js
+++ app.js
@@ -1,26 +1,20 @@
 var request = require('request');
-var pontos = require('./itinerarios/rota1.json');
+var rotas = [require('./itinerarios/rota1.json'), require('./itinerarios/rota2.json')];
 var i = 0;
-
-var options = {
-	uri: 'http://localhost:3000/itinerario_realizados',
- 	method: 'POST',
- 	form: {
- 		'authenticity_token': '09pnVsVNpbwUl765ni+owsy4L4PrKV1XW4cEF53u9MI=',
- 		'itinerario_realizado[latitude]': pontos[i].latitude,
- 		'itinerario_realizado[longitude]': pontos[i].longitude,
- 		'itinerario_realizado[date]': new Date(),
- 		'itinerario_realizado[viagem_id]': 1
-	}
-}
+var j = 0;
 
 var callback = function(error, response, body) {
 	if(error) {
 		console.log(error);
 	} else {
 		i++;
-		if(i < pontos.length){
+		if(i < rotas[j].length && j < rotas.length){
 			setTimeout(controle, 500);
+		} else if(j < rotas.length) {
+			i = 0;
+			j++;
+			setTimeout(controle, 500);
+			console.log('mudou de arquivo de rota');
 		}
 	}
 } 
@@ -31,10 +25,10 @@ var controle = function() {
 	 	method: 'POST',
 	 	form: {
 	 		'authenticity_token': '09pnVsVNpbwUl765ni+owsy4L4PrKV1XW4cEF53u9MI=',
-	 		'itinerario_realizado[latitude]': pontos[i].latitude,
-	 		'itinerario_realizado[longitude]': pontos[i].longitude,
+	 		'itinerario_realizado[latitude]': rotas[j][i].latitude,
+	 		'itinerario_realizado[longitude]': rotas[j][i].longitude,
 	 		'itinerario_realizado[date]': new Date(),
-	 		'itinerario_realizado[viagem_id]': 1
+	 		'itinerario_realizado[viagem_id]': (j + 1)
 		}
 	}
 	request(options, callback);
